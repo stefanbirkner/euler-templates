38c7f18ad9452276d89ce7a40e28c4c3e36bf607
Stefan Birkner <mail@stefan-birkner.de>
2013-04-02 22:25:09 +0200

    Add template for problems with Pythagorean triplets.

---
+++ PythagoraenTriplet.java
@@ -0,0 +1,9 @@
+public class ProblemX {
+	public static void main(String... args) {
+		System.out.println();
+	}
+
+	private static boolean isPythagoreanTriplet(int a, int b, int c) {
+		return (a * a) + (b * b) == (c * c);
+	}
+}
